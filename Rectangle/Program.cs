08393bb93d0c29293cced1bf435eb6e2c3220be3
p0melo11 <dimabob463@gmail.com>
2022-10-27 23:56:19 +0300

    1

---
+++ Rectangle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Перша сторона");
+            var side1 = Console.ReadLine();
+
+            Console.WriteLine("Друга сторона");
+            var side2 = Console.ReadLine();
+
+            var rectangle = new Rectangle(double.Parse(side1), double.Parse(side2));
+
+            Console.WriteLine($"Площа: {rectangle.Area}");
+            Console.WriteLine($"Периметр: {rectangle.Perimeter}");
+        }
+    }
+}
---
+++ Rectangle/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    public class Rectangle
+    {
+        private double side1;
+        private double side2;
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+        double AreaCalculator()
+        {
+            return this.side1 * this.side2;
+        }
+        double PerimeterCalculator()
+        {
+            return 2 * (this.side1 + this.side2);
+        }
+        public double Area
+        {
+            get { return this.AreaCalculator(); }
+        }
+        public double Perimeter
+        {
+            get { return this.PerimeterCalculator(); }
+        }
+    }
+}
